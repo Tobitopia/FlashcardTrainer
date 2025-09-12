21a24f04f268797a8e24569aa7138a37b3a61f66
TobiasBurkhardt99 <148750575+TobiasBurkhardt99@users.noreply.github.com>
2025-09-12 22:33:18 +0200

    feat: Enabled switching in navigation bar and set up screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:projects/card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,33 +10,87 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text('MyVocabTrainer')),
-      body: Center(child: Text('Hello')),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: const Icon(Icons.add),
-      ),
+    return MaterialApp(home: NavigationBarScreen());
+  }
+}
+
+class NavigationBarScreen extends StatefulWidget {
+  const NavigationBarScreen({super.key});
+
+  @override
+  State<NavigationBarScreen> createState() => _NavigationBarScreen();
+}
+
+class _NavigationBarScreen extends State<NavigationBarScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    SetsScreen(),
+    AllCardsScreen(),
+    StatsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Learning to switch states')),
+      body: Center(child: _widgetOptions.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
-          fixedColor: Colors.green,
-          items: const [
-            BottomNavigationBarItem(
-              label: "Sets",
-              icon: Icon(Icons.account_balance_wallet),
-            ),
-            BottomNavigationBarItem(
-              label: "Cards",
-              icon: Icon(Icons.add_card),
-            ),
-            BottomNavigationBarItem(
-              label: "Training",
-              icon: Icon(Icons.directions_run),
-            ),
-          ],
-            onTap: (int indexOfItem) {}),
-    ),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.folder), label: 'Sets'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card_outlined),
+            label: 'Cards',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.stacked_bar_chart),
+            label: 'Stats',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
     );
+  }
+}
+
+class SetsScreen extends StatelessWidget {
+  const SetsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          ListTile(title: Text("Example Set 1")),
+          ListTile(title: Text("Example Set 2")),
+        ],
+      ),
+    );
+  }
+}
+
+class AllCardsScreen extends StatelessWidget {
+  const AllCardsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("All cards will go here"));
+  }
+}
+
+class StatsScreen extends StatelessWidget {
+  const StatsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Stats will go here"));
   }
 }
