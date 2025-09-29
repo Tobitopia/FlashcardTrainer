5639971da5744f30a9fd3d49eec20a56096dc64a
TobiasBurkhardt99 <148750575+TobiasBurkhardt99@users.noreply.github.com>
2025-09-29 22:49:01 +0200

    refactor: clear layers: entrypoint, UI, models, and features

---
+++ lib/screens/all_cards/all_cards_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AllCardsScreen extends StatelessWidget {
+  const AllCardsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("All cards will go here"));
+  }
+}
