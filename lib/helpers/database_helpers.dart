580463e6ca822ad94b6447285f5f6797ffbc0b42
TobiasBurkhardt99 <148750575+TobiasBurkhardt99@users.noreply.github.com>
2025-10-01 23:16:10 +0200

    docs: Setting up structure for database and dependecies #1

---
+++ lib/helpers/database_helpers.dart
@@ -0,0 +1,13 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+
+class DatabaseHelper {
+  // How can you make sure you only ever have one instance of this class?
+  // static final ...
+
+  // You'll also need a variable to hold the database itself once it's opened.
+  // static sql.Database? _database;
+
+  // Next, you'll need a method to open (or create) the database.
+  // Future<sql.Database> get database async { ... }
+}
