dfc1da725771899f934d57cb12db218145edc9db
TobiasBurkhardt99 <148750575+TobiasBurkhardt99@users.noreply.github.com>
2025-10-04 23:13:31 +0200

    feat: cards and set have now methods to allow transformat to database sql format and back  #1

--- lib/models/vocab_card.dart
+++ lib/models/vocab_card.dart
@@ -1,16 +1,27 @@
 class VocabCard {
+  int? id;
   String front;
   String back;
-  final List<String> labels;
+  final List<String> labels; // Now active
   int rating;
 
   // constructor
-  VocabCard({
+  VocabCard({    this.id,
     required this.front,
     required this.back,
-    this.labels = const [],
+    this.labels = const [], // Now active, defaults to an empty list
     this.rating = 0,
   });
+
+  // The toMap() method does NOT change. We don't save the labels list directly.
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'front': front, 'back': back, 'rating': rating};
+  }
+
+  // The fromMap() method creates a card, but the labels will be added later.
+  factory VocabCard.fromMap(Map<String, dynamic> map) {
+    return VocabCard(id: map['id'], front: map['front'], back: map['back'], rating: map['rating']);
+  }
 
   void updateRating(int newRating){
     rating = newRating;
--- lib/models/vocab_set.dart
+++ lib/models/vocab_set.dart
@@ -1,14 +1,25 @@
 import 'vocab_card.dart';
 
 class VocabSet{
+  int? id;
   final String name;
   List<VocabCard> cards;
 
   // constructor
   VocabSet({
+    this.id,
     required this.name,
     List<VocabCard>? cards,
   }): cards = cards ?? [];
+
+  Map<String, Object?> toMap() {
+    return {'name': name};
+  }
+
+  factory VocabSet.fromMap(Map<String, dynamic> map) {
+    return VocabSet(id: map['id'], name: map['name']);
+  }
+
 
   void addCard(VocabCard card){
     cards.add(card);
